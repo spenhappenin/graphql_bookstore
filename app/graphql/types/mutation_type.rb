bfe05924b2902d0b28625542b49e0bedf5b9e294
Spencer Richards <spencer.richards7@gmail.com>
2018-08-27 11:33:11 -0600

    all book mutations complete

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,5 +1,11 @@
 class Types::MutationType < Types::BaseObject
+  # Authors
   field :create_author, mutation: Mutations::CreateAuthor
   field :update_author, mutation: Mutations::UpdateAuthor
   field :delete_author, mutation: Mutations::DeleteAuthor
+
+  # Books
+  field :create_book, mutation: Mutations::CreateBook
+  field :update_book, mutation: Mutations::UpdateBook
+  field :delete_book, mutation: Mutations::DeleteBook
 end
