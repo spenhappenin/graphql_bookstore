e25348acd0beca178e3ae7c9c1592b28606a3eec
Spencer Richards <spencer.richards7@gmail.com>
2018-08-27 09:55:56 -0600

    finish author query types

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -4,8 +4,17 @@ class Types::QueryType < Types::BaseObject
   field :authors, [AuthorType], null: true do
     description "gets all authors"
   end
+
+  field :author, AuthorType, null: true do
+    description "Find an author by id"
+    argument :id, ID, required: true
+  end
   
   def authors
     Author.all
+  end
+
+  def author(id:)
+    Author.find(id)
   end
 end
