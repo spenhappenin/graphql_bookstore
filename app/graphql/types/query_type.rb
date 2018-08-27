d3a23548eec051f13efd5b1d95113cb77288a613
Spencer Richards <spencer.richards7@gmail.com>
2018-08-27 09:51:58 -0600

    graphql set up and query for authors working

---
+++ app/graphql/types/query_type.rb
@@ -0,0 +1,11 @@
+class Types::QueryType < Types::BaseObject
+  description "root query"
+  
+  field :authors, [AuthorType], null: true do
+    description "gets all authors"
+  end
+  
+  def authors
+    Author.all
+  end
+end
