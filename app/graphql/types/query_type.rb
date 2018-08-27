463eb5915748780f8bc5335bed96b2f39afadd2f
Spencer Richards <spencer.richards7@gmail.com>
2018-08-27 10:07:11 -0600

    finish all querries

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,12 +1,23 @@
 class Types::QueryType < Types::BaseObject
   description "root query"
   
+  # -- Authors --
   field :authors, [AuthorType], null: true do
     description "gets all authors"
   end
 
   field :author, AuthorType, null: true do
     description "Find an author by id"
+    argument :id, ID, required: true
+  end
+
+  # -- Books --
+  field :books, [BookType], null: true do
+    description "Get all books"
+  end
+
+  field :book, BookType, null: true do
+    description "Find a book by id"
     argument :id, ID, required: true
   end
   
@@ -16,5 +27,13 @@ class Types::QueryType < Types::BaseObject
 
   def author(id:)
     Author.find(id)
+  end
+
+  def books
+    Book.all
+  end
+
+  def book(id:)
+    Book.find(id)
   end
 end
